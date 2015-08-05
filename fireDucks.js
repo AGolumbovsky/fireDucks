76d5413f3a3b8455f7ddaa69d69c0c94c0fc44bd
AGolumbovsky <agolumbovsky@gmail.com>
2015-08-04 23:56:02 -0500

    older fireDucks code

---
+++ fireDucks.js
@@ -0,0 +1,7 @@
+var fdRef = new Firebase('https://amber-heat-3881.firebaseio.com/');
+
+$('#messageIn').keypress(function() {
+
+})
+
+// 	NODE SERVER. SEPARATE LATER
---
+++ server.js
@@ -0,0 +1,6 @@
+
+var express = require('express');
+var fireBase = require('firebase');
+
+// firebase url
+var fireDuck = new Firebase('https://amber-heat-3881.firebaseio.com/')
