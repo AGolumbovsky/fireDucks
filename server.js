985f5b6fa13ce11d9cbdfe623b1c143e17e64fb4
AGolumbovsky <agolumbovsky@gmail.com>
2015-08-05 23:18:59 -0500

    ignore the node module

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 
 var express = require('express');
-var fireBase = require('firebase');
+var firebase = require('firebase');
 
 // firebase url
-var fireDuck = new Firebase('https://amber-heat-3881.firebaseio.com/')
+var fdRef = new firebase('https://amber-heat-3881.firebaseio.com/')
+
+var app = express();
+
+
+
+//start servers
+app.listen(8181, function() {
+	console.log("listening on 8181")
+})
