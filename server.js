90129058a4b4b6179038bfd57946dda1d003ade9
AGolumbovsky <agolumbovsky@gmail.com>
2015-10-01 18:21:15 -0500

    ff

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ var express = require('express');
 var firebase = require('firebase');
 
 // firebase url
-var fdRef = new firebase('https://amber-heat-3881.firebaseio.com/')
+var fdRef = new firebase('https://amber-heat-3881.firebaseio.com/');
+fdRef.set("High their")
 
 var app = express();
 app.get(fdRef, funcion(req, res) {
 	console.log('did you get it?')
-}
+})
 
 //start servers
 app.listen(8181, function() {
